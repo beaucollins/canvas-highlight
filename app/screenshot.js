63d99324e59ff0e75b5b5de929a012defb45c59f
Robert Collins <beau@collins.pub>
2017-02-15 13:06:32 -0800

    polyfill HTMLCanvasElement.toBlob

--- app/screenshot.js
+++ app/screenshot.js
@@ -235,21 +235,20 @@ if ( window ) {
 	window.screenshot = init;
 }
 
-if (!HTMLCanvasElement.prototype.toBlob) {
- Object.defineProperty(HTMLCanvasElement.prototype, 'toBlob', {
-  value: function (callback, type, quality) {
+if ( !HTMLCanvasElement.prototype.toBlob ) {
+	Object.defineProperty( HTMLCanvasElement.prototype, 'toBlob', {
+		value: function( callback, type, quality ) {
+			var binStr = atob( this.toDataURL( type, quality ).split( ',' )[1] ),
+				len = binStr.length,
+				arr = new Uint8Array( len );
 
-    var binStr = atob( this.toDataURL(type, quality).split(',')[1] ),
-        len = binStr.length,
-        arr = new Uint8Array(len);
+			for ( let i = 0; i < len; i++ ) {
+				arr[i] = binStr.charCodeAt( i );
+			}
 
-    for (var i=0; i<len; i++ ) {
-     arr[i] = binStr.charCodeAt(i);
-    }
-
-    callback( new Blob( [arr], {type: type || 'image/png'} ) );
-  }
- });
+			callback( new Blob( [arr], {type: type || 'image/png'} ) );
+		}
+	} );
 }
 
 export default init;
